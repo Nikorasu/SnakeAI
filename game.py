292c543345d987b936061d9aaee1d2019cf878c9
Nikorasu <nikorasu85@gmail.com>
2023-02-06 00:15:57 -0600

    Simplified input actions down to 3, faster methods

--- game.py
+++ game.py
@@ -4,25 +4,26 @@
 # This version is not meant to be played by a human, I hope to try to train an AI to play it.
 # Copyright (C) 2022  Nik Stromberg - nikorasu85@gmail.com
 
-import random as rnd, numpy as np
+import numpy as np
 
 SIZE = (20, 20)
 
 class Snake:
     def __init__(self, size=SIZE):
         self.rows, self.cols = size
-        self.pos = np.array([self.rows//2-1, self.cols//2-1], int) # start position
-        self.dir = np.array(rnd.choice([(-1,0),(1,0),(0,-1),(0,1)]), int) # random direction
+        self.pos = np.array((self.rows//2-1, self.cols//2-1), int) # start position
+        self.actdir = np.random.randint(0,3)
+        self.dir = np.array(((-1,0),(0,1),(1,0),(0,-1))[self.actdir], int) # random direction (up, right, down, left)
         self.segments = np.array([self.pos], int) # array of previous positions
         self.len = 3 # starting length of snake
-        self.food = np.array([rnd.randint(0,self.rows-1),rnd.randint(0,self.cols-1)], int) # random food position
-        self.state = np.zeros((self.rows,self.cols), int)
+        self.food = np.array((np.random.randint(self.rows), np.random.randint(self.cols)), int) # random food position
+        self.state = np.zeros((self.rows, self.cols), int)
         self.state[self.food[0], self.food[1]] = 2
         self.state[self.segments[:,0], self.segments[:,1]] = 1
         self.gameover = False
-    def update(self, newdir=None):
-        if newdir and np.all(newdir != -self.dir): # if new direction is not opposite of current direction
-            self.dir = np.array(newdir, int) # update direction
+    def update(self, action=1): # left -1, straight 0, right 1 (take 0=left, 1=straight, 2=right then -1 to convert)
+        self.actdir = (self.actdir + (action-1)) % 4
+        self.dir = ((-1,0),(0,1),(1,0),(0,-1))[self.actdir]
         hitself = np.all(self.pos + self.dir == self.segments[1:], axis=1).any()
         if np.any(self.pos + self.dir < 0) or np.any(self.pos + self.dir >= [self.rows, self.cols]) or hitself:
             self.gameover = True; return # if snake hits border or itself, game over
@@ -30,9 +31,9 @@ class Snake:
         self.segments = np.concatenate([[self.pos], self.segments[:self.len-1]]) # add new position to segments
         if np.all(self.pos == self.food): # if snake eats food
             self.len += 1 # increase length of snake
-            field = np.array(np.meshgrid(np.arange(self.rows), np.arange(self.cols))).T.reshape(-1, 2)
-            field = np.array([p for p in field if not np.any(np.all(p == self.segments, axis=1))])
-            self.food = field[np.random.choice(len(field))] # random food position that is not on snake
+            field = {(x, y) for x in range(self.rows) for y in range(self.cols)}
+            freespace = list(field - set(map(tuple, self.segments)))
+            self.food = freespace[np.random.randint(len(freespace))]
         self.state[:] = 0 # reset state
         self.state[self.food[0], self.food[1]] = 2 # update food position
         self.state[self.segments[:,0], self.segments[:,1]] = 1 # update snake positions
@@ -44,11 +45,10 @@ def main():
         print('\x1b[2J\x1b[H\x1b]0;Snake',end='\a',flush=True)
         player = Snake()
         print(player.state)
-        directions = {'w':(-1,0),'s':(1,0),'a':(0,-1),'d':(0,1)} # key directions
         while (action:=input()) != 'q': # will need to be changed to AI input later
             start = time.perf_counter()
             print('\x1b[0m\x1b[2J\x1b[H',end='',flush=True) # clear screen
-            player.update(directions.get(action)) # update player with the direction of key pressed
+            player.update(1 if action == '' else int(action)) # 0=left, 1=straight, 2=right
             print(player.state)
             end = time.perf_counter()
             howlong = end - start
